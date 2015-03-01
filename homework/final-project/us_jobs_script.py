1e873500cd274534ecad9433138bc59c57f6a9d6
Ariha Setalvad <Ariha@Arihas-MacBook-Pro.local>
2015-03-01 02:11:50 -0800

    print job summaries

--- homework/final-project/us_jobs_script.py
+++ homework/final-project/us_jobs_script.py
@@ -1,12 +1,21 @@
 from os import listdir
 from os.path import isfile, join
+import os
+import json
 
-
-mypath=listdir(".")[1]
+mypath=os.path.expanduser("~/compciv/homework/usjobsgov/data-hold/scrapes/2015-02-28_1500")
+# for loop that picks out regular files within specified directory
 onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath, f)) ]
 
 # >>> onlyfiles[0]
 # '0000-1.json'
 p = join (mypath, onlyfiles[0])
+print p
 f = open (p, "r")
+print f
 lines = f.read()
+j=json.loads (lines)
+#j ['JobData'][0].keys()
+cyber_jobs = []
+for job in j["JobData"]:
+	print job["JobSummary"]
